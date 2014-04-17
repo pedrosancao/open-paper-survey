f6e3093fee15608b9f7625b8446bc11ee3758c06
Pedro Sanção <pedro@sancao.co>
2014-04-17 11:10:40 -0300

    code refactoring of barcode functions to an OOP aproach, keeping backward compatibility

--- functions/functions.barcode.php
+++ functions/functions.barcode.php
@@ -25,239 +25,294 @@
 include_once(dirname(__FILE__).'/../config.inc.php');
 
 /**
- * Given a 1 bit image containing a barcode
- * Return an array of bar widths
- */
-function barWidth($image, $scany) {
-	$xdim = imagesx($image);
-	$b = array();
-	$count = 0;
-
-	$col = imagecolorat($image, 0, $scany);
-	for ($x = 0; $x < $xdim; $x++) {
-		$rgb = imagecolorat($image, $x, $scany);
-		if ($rgb != $col) {
-			$b[] = $count;
-			//$b[]['colour'] = $rgb;
-			$count = 0;
-			$col = $rgb;
-			//print("$col<br/>");
-		}
-		$count++;	
-	}
-	return $b;
-}
-
-/**
- * Given an array of widths, return the guess of
- * the width of narrow and wide bars
- */
-function nwWidth($array) {
-	$a = array();
-	sort($array);
-	$elements = count($array);
-
-	if ($elements <= 1) {
-		$a['n'] = 0;
-		$a['w'] = 0;
-		return $a;
-	}
-
-	$a['n'] = $array[(($elements/4)+1)];
-	$a['w'] = $array[($elements-(($elements/4)+1))];
-
-	//print ("N: {$a['n']} W: {$a['w']}<br/>");
-
-	return $a;
-}
-
-/**
- * Given an array of widths, an estimate of the widths
- * of wide and narrow bars, return narrow/wide rep as string
- */
-function widthsToNW($widths,$narrow,$wide) {
-	//give a large tolerance
-
-	$tolerance = (($wide - $narrow) - 1) / 2;
-	$string = '';
-
-	$nmin = ($narrow - $tolerance);
-	if ($nmin <= 0) {
-		$nmin = 1;
-	}
-
-	foreach($widths as $width) {
-		if (($width >= ($nmin)) && ($width <= ($narrow + $tolerance))) {
-			$string .= "N";
-		}
-		elseif (($width >= ($wide - $tolerance)) && ($width <= ($wide + $tolerance))) {
-			$string .= "W";
-		}
-		else {
-			$string .= "J"; //J for junk
-		}
-	}
-
-	//remove junk bits from start and end of string
-	$firstJ = strpos($string, 'J');
-	if ($firstJ <= ((strlen($string) / 4))) {
-		$string = substr($string, $firstJ + 1);
-	}
-
-	$lastJ = strpos($string, 'J', ((strlen($string) / 4) * 3));
-	if ($lastJ >= ((strlen($string) / 4) * 3)) {
-		$string = substr($string, 0, $lastJ);
-	}
-
-	return $string;
-}
-
-/**
- * Given a string of n's and w's return a code (for CodaBar)
- * Bar widthds sourced from: http://www.barcodesymbols.com/codabar.htm
+ * Barcode functions
  *
- * Added an extra "N" to the end of each to account for intercode spacing (always narrow)
+ * @author Pedro Sanção <pedro@sancao.com.br>
  */
-function NWtoCodeCodaBar($s) {
-	$hash = array();
+class Barcode {
 
-	$hash['NNNNNWWN'] = 0;
-	$hash['NNNNWWNN'] = 1;
-	$hash['NNNWNNWN'] = 2;
-	$hash['WWNNNNNN'] = 3;
-	$hash['NNWNNWNN'] = 4;
-	$hash['WNNNNWNN'] = 5;
-	$hash['NWNNNNWN'] = 6;
-	$hash['NWNNWNNN'] = 7;
-	$hash['NWWNNNNN'] = 8;
-	$hash['WNNWNNNN'] = 9;
-	$hash['NNNWWNNN'] = '-';
-	$hash['NNWWNNNN'] = '$';
-	$hash['WNNNWNWN'] = ':';
-	$hash['WNWNNNWN'] = '/';
-	$hash['WNWNWNNN'] = '.';
-	$hash['NNWNWNWN'] = '+';
-	$hash['NNWWNWNN'] = 'A';
-	$hash['NWNWNNWN'] = 'B';
-	$hash['NNNWNWWN'] = 'C';
-	$hash['NNNWWWNN'] = 'D';
-	
-	$code = "";
-	for ($i = 0; $i < strlen($s); $i+= 8) {
-		$b1 = substr($s,$i,8);
-		if (!isset($hash[$b1])) {
-			return "false";
+	/**
+	 * Find the bars widths
+	 *
+	 * @param resource $image A 1 bit image (of type GD) containing a barcode
+	 * @param int $y Y coordinate to read
+	 * @return array An array of bar widths
+	 */
+	private static function getBarWidths($image, $y) {
+		$width = imagesx($image);
+		$widths = array();
+		$count = 0;
+		$color = imagecolorat($image, 0, $y);
+		for ($x = 0; $x < $width; $x++) {
+			$rgb = imagecolorat($image, $x, $y);
+			if ($rgb != $color) {
+				$widths[] = $count;
+				$count = 0;
+				$color = $rgb;
+			}
+			$count++;	
 		}
-		$code .= $hash[$b1];
+		return $widths;
 	}
 
-	return $code;
-}
-
-/**
- * Given a string of n's and w's return a code (for Interleaved 2 of 5)
- */
-function NWtoCode($s) {
-	$hash = array();
-	$hash['NNWWN'] = 0;
-	$hash['WNNNW'] = 1;
-	$hash['NWNNW'] = 2;
-	$hash['WWNNN'] = 3;
-	$hash['NNWNW'] = 4;
-	$hash['WNWNN'] = 5;
-	$hash['NWWNN'] = 6;
-	$hash['NNNWW'] = 7;
-	$hash['WNNWN'] = 8;
-	$hash['NWNWN'] = 9;
-
-	$code = "";
-	//ignore the first 4 and last 3
-	for ($i = 4; $i < (strlen($s) - 3); $i+= 10) {
-		$b1 = $s[$i] . $s[$i + 2] . $s[$i + 4] . $s[$i + 6] . $s[$i + 8];
-		$b2 = $s[$i + 1] . $s[$i + 3] . $s[$i + 5] . $s[$i + 7] . $s[$i + 9];
-		if (!isset($hash[$b1]) || !isset($hash[$b2])) {
-			return "false";
+	/**
+	 * Guess the width of narrow and wide bars
+	 *
+	 * @param array $widths An array of bars widths
+	 * @return array An array containg the width of narrow and wide bars
+	 */
+	private static function getNarrowWideWidth(array $widths) {
+		$elements = count($widths);
+		if ($elements < 4) {
+			return array(
+				'n' => 0,
+				'w' => 0,
+			);
 		}
-		$code .= $hash[$b1] . $hash[$b2];
+
+		sort($widths);
+		return array(
+			'n' => $widths[round($elements / 4) + 1],
+			'w' => $widths[$elements - round($elements / 4) + 1],
+		);
 	}
 
-	return $code;
-}
+	/**
+	 * Get the narrow/wide representation as string
+	 * 
+	 * @param array $widths An array of bars widths
+	 * @param int $narrow An estimate width of narrow bars
+	 * @param int $wide An estimate width of wide bars
+	 * @return string narrow/wide representation
+	 */
+	private static function widthsToNarrowWide(array $widths, $narrow, $wide) {
+		//give a large tolerance
 
-function validate($s) {
-	//length must be 10 * count + 7
-	//must start with nnnn
-	//must end with wnn
-	
-	if ((fmod((strlen($s)-7.0),10.0) == 0) && (strncmp($s,"NNNN",4) == 0) && (strncmp(substr($s,(strlen($s)-3),3),"WNN",3) == 0)) {
+		$tolerance = (($wide - $narrow) - 1) / 2;
+		$string = '';
+
+		$nmin = ($narrow - $tolerance);
+		if ($nmin <= 0) {
+			$nmin = 1;
+		}
+
+		foreach($widths as $width) {
+			if (($width >= ($nmin)) && ($width <= ($narrow + $tolerance))) {
+				$string .= "N";
+			}
+			elseif (($width >= ($wide - $tolerance)) && ($width <= ($wide + $tolerance))) {
+				$string .= "W";
+			}
+			else {
+				$string .= "J"; //J for junk
+			}
+		}
+
+		//remove junk bits from start and end of string
+		$firstJ = strpos($string, 'J');
+		if ($firstJ <= ((strlen($string) / 4))) {
+			$string = substr($string, $firstJ + 1);
+		}
+
+		$lastJ = strpos($string, 'J', ((strlen($string) / 4) * 3));
+		if ($lastJ >= ((strlen($string) / 4) * 3)) {
+			$string = substr($string, 0, $lastJ);
+		}
+
+		return $string;
+	}
+
+	/**
+	 * Validate an interleaved 2 of 5 barcode
+	 * 
+	 * @param string $bars narrow/wide representation of barcode
+	 * @return boolean
+	 */
+	private static function validateI25($bars) {
+		// length must be 10 * length + 7
+		if (fmod(strlen($bars), 10) != 7) {
+			return false;
+		}
+
+		// must start with nnnn
+		if (strncmp($bars, 'NNNN', 4) !== 0) {
+			return false;
+		}
+
+		// must end with wnn
+		if (substr($bars, -3) !== 'WNN') {
+			return false;
+		}
+
 		return true;
 	}
 
-	return false;
+	/**
+	 * Get a interleaved 2 of 5 code
+	 * 
+	 * @param string $bars narrow/wide representation of barcode
+	 * @return string the value of code of 'false' on failure
+	 */
+	private static function getCodeI25($bars) {
+		$conversionTable = array(
+			'NNWWN' => 0,
+			'WNNNW' => 1,
+			'NWNNW' => 2,
+			'WWNNN' => 3,
+			'NNWNW' => 4,
+			'WNWNN' => 5,
+			'NWWNN' => 6,
+			'NNNWW' => 7,
+			'WNNWN' => 8,
+			'NWNWN' => 9,
+		);
 
-}
-
-function validateCodaBar($s) {
-	//length + 1 must be a multiple of 8 (each character is 7 bars/spaces and a space)
-	//must start and end with a start/stop character (A,B,C or D)
-	
-	if ((fmod((strlen($s) + 1),8.0) == 0)) {
-		$start = NWtoCodeCodaBar(substr($s,0,8));
-		$end = NWtoCodeCodaBar(substr($s,-7) . "N");
-		if (($start == 'A' || $start  == 'B' || $start == 'C' || $start == 'D') && ($end == 'A' || $end  == 'B' || $end == 'C' || $end == 'D')) {
-			return true;	
-		}
-	}
-	return false;
-}
-
-/**
- * Given a GD image, Find an interleaved 2 of 5 barcode and return it otherwise
- * return false
- *
- * Currently steps pixel by pixel (step = 1)
- *
- */
-function barcode($image, $step = 1, $length = false) {
-	if (function_exists('imagefilter') &&
-		function_exists('imagetruecolortopalette') &&
-		function_exists('imagecolorset') &&
-		function_exists('imagecolorclosest'))
-	{
-		//Gaussian blur to fill in holes from dithering
-		imagefilter($image, IMG_FILTER_GAUSSIAN_BLUR);
-		//force two colors; dithering = FALSE
-		imagetruecolortopalette($image, FALSE, 2);
-		//find the closest color to black and replace it with actual black
-		imagecolorset($image, imagecolorclosest($image, 0, 0, 0), 0, 0, 0);
-		//find the closest color to white and replace it with actual white
-		imagecolorset($image, imagecolorclosest($image, 255, 255, 255), 255, 255, 255);
-	}
-	//search
-
-	$height = imagesy($image);
-
-	for ($i = ($step); $i < $height - ($step); $i += ($step)) {
-		$a = barWidth($image,$i);
-		$w = nwWidth($a);
-		if ($w['n'] != 0 && $w['w'] != 0) {
-			$s = widthsToNW($a, $w['n'], $w['w']);
-			if(validate($s)){
-				$code = NWtoCode($s);
-				if ($code != "false" && (!$length || strlen($code) == $length)) {
-					return $code;
-				}
+		$code = '';
+		// ignore the first 4 and last 3
+		for ($i = 4; $i < strlen($bars) - 3; $i += 10) {
+			$b1 = $bars[$i] . $bars[$i + 2] . $bars[$i + 4] . $bars[$i + 6] . $bars[$i + 8];
+			$b2 = $bars[$i + 1] . $bars[$i + 3] . $bars[$i + 5] . $bars[$i + 7] . $bars[$i + 9];
+			if (!isset($conversionTable[$b1]) || !isset($conversionTable[$b2])) {
+				return 'false';
 			}
-			elseif (validateCodaBar($s)) {
-				$code = NWtoCodeCodaBar($s . "N"); //remember to add the last space
-				if ($code != "false") {
-					$code = substr($code,1,-1); //remove the start and stop characters
-					if (!$length || strlen($code) == $length) {
+			$code .= $conversionTable[$b1] . $conversionTable[$b2];
+		}
+
+		return $code;
+	}
+
+	/**
+	 * Validate a codabar barcode
+	 * 
+	 * @param string $bars narrow/wide representation of barcode
+	 * @return boolean
+	 */
+	private static function validateCodabar($bars) {
+		// length + 1 must be a multiple of 8 (each character is 7 bars/spaces and a space)
+		if (fmod(strlen($bars) + 1, 8) != 0) {
+			return false;
+		}
+
+		// must start and end with a stop character (A, B, C or D)
+		$stopChars = range('A', 'D');
+		$start = self::getCodabar(substr($bars, 0, 8));
+		if (!in_array($start, $stopChars)) {
+			return false;
+		}
+		$end = self::getCodabar(substr($bars, -7) . 'N');
+		if (!in_array($end, $stopChars)) {
+			return false;
+		}
+		
+		return true;
+	}
+
+	/**
+	 * Get a codabar code
+	 *
+	 * @see http://www.barcodesymbols.com/codabar.htm
+	 *
+	 * @param string $bars narrow/wide representation of barcode
+	 * @return string the value of code of 'false' on failure
+	 */
+	private static function getCodabar($bars) {
+		$conversionTable = array(
+			'NNNNNWWN' => 0,
+			'NNNNWWNN' => 1,
+			'NNNWNNWN' => 2,
+			'WWNNNNNN' => 3,
+			'NNWNNWNN' => 4,
+			'WNNNNWNN' => 5,
+			'NWNNNNWN' => 6,
+			'NWNNWNNN' => 7,
+			'NWWNNNNN' => 8,
+			'WNNWNNNN' => 9,
+			'NNNWWNNN' => '-',
+			'NNWWNNNN' => '$',
+			'WNNNWNWN' => ':',
+			'WNWNNNWN' => '/',
+			'WNWNWNNN' => '.',
+			'NNWNWNWN' => '+',
+			'NNWWNWNN' => 'A',
+			'NWNWNNWN' => 'B',
+			'NNNWNWWN' => 'C',
+			'NNNWWWNN' => 'D',
+		);
+
+		$code = '';
+		for ($i = 0; $i < strlen($bars); $i+= 8) {
+			$bar = substr($bars, $i, 8);
+			if (!isset($conversionTable[$bar])) {
+				return 'false';
+			}
+			$code .= $conversionTable[$bar];
+		}
+
+		return $code;
+	}
+
+	/**
+	 * Search and read an interleaved 2 of 5 or a codabar barcode
+	 * 
+	 * @param resource $image image of type GD
+	 * @param int $step pixels to read
+	 * @param int $length length of barcode
+	 * @return string the found barcode or false
+	 */
+	public static function read($image, $step = 1, $length = BARCODE_LENGTH) {
+		if (function_exists('imagefilter') &&
+			function_exists('imagetruecolortopalette') &&
+			function_exists('imagecolorset') &&
+			function_exists('imagecolorclosest'))
+		{
+			// Gaussian blur to fill in holes from dithering
+			imagefilter($image, IMG_FILTER_GAUSSIAN_BLUR);
+			// force two colors; dithering = FALSE
+			imagetruecolortopalette($image, FALSE, 2);
+			// find the closest color to black and replace it with actual black
+			imagecolorset($image, imagecolorclosest($image, 0, 0, 0), 0, 0, 0);
+			// find the closest color to white and replace it with actual white
+			imagecolorset($image, imagecolorclosest($image, 255, 255, 255), 255, 255, 255);
+		}
+
+		$height = imagesy($image);
+
+		for ($y = $step; $y < $height - $step; $y += $step) {
+			$widths = self::getBarWidths($image, $y);
+			$barWidth = self::getNarrowWideWidth($widths);
+			if ($barWidth['n'] != 0 && $barWidth['w'] != 0) {
+				$bars = self::widthsToNarrowWide($widths, $barWidth['n'], $barWidth['w']);
+				if(self::validateI25($bars, $image, $y)) {
+					$code = self::getCodeI25($bars);
+					if ($code != "false" && (!$length || strlen($code) == $length)) {
 						return $code;
+					}
+				}
+				elseif (self::validateCodabar($bars)) {
+					$code = NWtoCodeCodaBar($bars . 'N'); // add the last space
+					if ($code != "false") {
+						$code = substr($code, 1, -1); // remove the start and stop characters
+						if (!$length || strlen($code) == $length) {
+							return $code;
+						}
 					}
 				}
 			}
 		}
+		return false;
 	}
-	return false;
+
+}
+
+/**
+ * Search and read an interleaved 2 of 5 or a codabar barcode
+ *
+ * @param resource $image image of type GD
+ * @param int $step pixels to read
+ * @param int $length length of barcode
+ * @return string the found barcode or false
+ */
+function barcode($image, $step = 1, $length = false) {
+	$params = func_get_args();
+	call_user_func_array('Barcode::read', $params);
 }
